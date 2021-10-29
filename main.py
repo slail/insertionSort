b285b88f141a9ddefa2dd89f62b09523ed31c940
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-29 16:34:58 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def insertionSort(array):
+    """
+    A function that takes in an array of integers and returns a sorted version of that array using the insertion sort
+    algorithm.
+    """
+    pass
