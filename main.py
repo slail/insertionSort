829803fbd5c9e4e5ba82abd2026b2421b6976f54
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-29 16:47:40 -0400

    added insertionSort algo

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ def insertionSort(array):
     A function that takes in an array of integers and returns a sorted version of that array using the insertion sort
     algorithm.
     """
-    pass
+    for idx in range(1, len(array)):
+        while idx > 0 and array[idx] < array[idx - 1]:
+            swap(idx, idx - 1, array)
+            idx -= 1
+    return array
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
